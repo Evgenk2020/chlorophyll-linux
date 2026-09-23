69021bdde7cf0e4bcc33621ac117371276b93645
evheny <evhenykopilov@ukr.net>
2026-09-23 16:18:27 +0300

    modified 	modified:   include/proc.h

--- include/proc.h
+++ include/proc.h
@@ -14,8 +14,8 @@ protected:
     bool _json_output{false};
 
     virtual void helping() = 0;
-    virtual std::expected<ch_data, std::string> parsing() = 0;
     virtual void counting(const ch_data &datas) = 0;
+    virtual std::expected<ch_data, std::string> parsing() = 0;
 
 public:
     virtual ~processing() = default;
@@ -34,8 +34,8 @@ public:
     decree(int argc, char **argv);
 
     void helping() override;
-    std::expected<ch_data, std::string> parsing() override;
     void counting(const ch_data &datas) override;
+    std::expected<ch_data, std::string> parsing() override;
 };
 
 #endif // PROC_H
